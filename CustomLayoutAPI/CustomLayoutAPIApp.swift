6ac1fe6f7a6038fd229111d8c8263a8d39f623b6
harsh <bighugscafe@gmail.com>
2025-03-17 19:21:33 +0530

    Initial Commit

---
+++ CustomLayoutAPI/CustomLayoutAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomLayoutAPIApp.swift
+//  CustomLayoutAPI
+//
+//  Created by harsh  on 17/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomLayoutAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
